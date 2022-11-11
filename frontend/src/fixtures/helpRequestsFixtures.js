9e2d82d3de9cd21780a1efc697709420e0194ee7
irenecho75 <irenecho75@gmail.com>
2022-11-10 18:09:50 -0800

    ic - Add help request table and tests

---
+++ frontend/src/fixtures/helpRequestsFixtures.js
@@ -0,0 +1,43 @@
+const helpRequestsFixtures = {
+    oneHelpRequest: {
+        "id": 1,
+        "requesterEmail": "irenecho@ucsb.edu",
+        "teamId": "6pm-4",
+        "tableOrBreakoutRoom": "10",
+        "requestTime": "2022-01-02T12:00:00",
+        "explanation": "need help with team02",
+        "solved": false
+    },
+    threeHelpRequests: [
+        {
+            "id": 1,
+            "requesterEmail": "irenecho@ucsb.edu",
+            "teamId": "6pm-4",
+            "tableOrBreakoutRoom": "10",
+            "requestTime": "2022-01-02T12:00:00",
+            "explanation": "need help with team02",
+            "solved": false
+        },
+        {
+            "id": 2,
+            "requesterEmail": "irenechoo@ucsb.edu",
+            "teamId": "6pm-1",
+            "tableOrBreakoutRoom": "2",
+            "requestTime": "2022-10-02T12:00:00",
+            "explanation": "need help with jpa02",
+            "solved": false
+        },
+        {
+            "id": 3,
+            "requesterEmail": "irenechooo@ucsb.edu",
+            "teamId": "5pm-1",
+            "tableOrBreakoutRoom": "4",
+            "requestTime": "2020-10-02T12:00:00",
+            "explanation": "need help with jpa03",
+            "solved": false
+        }
+    ]
+};
+
+
+export { helpRequestsFixtures };
