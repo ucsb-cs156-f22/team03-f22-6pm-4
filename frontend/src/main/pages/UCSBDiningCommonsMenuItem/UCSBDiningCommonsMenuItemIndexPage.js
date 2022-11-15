558f9df98ce69fa46b4c811aea18d25d3da97401
Vyom Singh <vyomsaritsingh@ucsb.edu>
2022-11-15 01:41:27 -0800

    vs - missing semicolon

--- frontend/src/main/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemIndexPage.js
+++ frontend/src/main/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemIndexPage.js
@@ -3,7 +3,7 @@ import { useBackend } from 'main/utils/useBackend'; // use prefix indicates a Re
 
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import UCSBDiningCommonsMenuItemTable from 'main/components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemTable';
-import { useCurrentUser } from 'main/utils/currentUser' // use prefix indicates a React Hook
+import { useCurrentUser } from 'main/utils/currentUser'; // use prefix indicates a React Hook
 
 export default function UCSBDiningCommonsMenuItemIndexPage() {
 
@@ -21,7 +21,7 @@ export default function UCSBDiningCommonsMenuItemIndexPage() {
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>UCSBDiningCommonsMenuItem</h1>
+        <h1>UCSB Dining Commons Menu Item</h1>
         <UCSBDiningCommonsMenuItemTable ucsbDiningcCommonsMenuItem={ucsbdiningcommonsmenuitem} currentUser={currentUser} />
       </div>
     </BasicLayout>
