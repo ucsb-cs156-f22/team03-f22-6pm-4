31af59ca993de2ef46e89468348b2df33f792f5d
BenYangSB <72826356+BenYangSB@users.noreply.github.com>
2022-11-16 13:48:58 -0800

    .

--- frontend/src/tests/components/Articles/ArticlesTable.test.js
+++ frontend/src/tests/components/Articles/ArticlesTable.test.js
@@ -1,11 +1,9 @@
-import { fireEvent, render, waitFor } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import ArticlesTable from "main/components/Articles/ArticlesTable";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 import { currentUserFixtures } from "fixtures/currentUserFixtures";
 import { articlesFixtures } from "fixtures/articlesFixtures";
-import ArticlesIndexPage from "main/pages/Articles/ArticlesIndexPage";
-
 import AxiosMockAdapter from "axios-mock-adapter";
 import axios from "axios";
 
